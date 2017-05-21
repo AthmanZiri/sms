7ef461427f52de371d38735c847d1898fb9e8791
athmanziri <issaziri@gmail.com>
2017-05-22 01:49:07 +0300

    batch update. changed UI

--- sms/urls.py
+++ sms/urls.py
@@ -3,8 +3,9 @@ from django.conf.urls import url
 from sms import views
 
 urlpatterns = [
+	
+	url(r'^list/', views.sms_list, name='sms_list'),
 
     url(r'^', views.sms_create, name='sms_create'),
     
-    url(r'^messages/', views.sms_list, name='sms_list'),
 ]
